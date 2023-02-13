38a07c6f48d4ec7c5c5bacfff1076a3327941400
albertying <albertlying@gmail.com>
2023-02-13 18:48:04 -0500

    Create get routes and controllers for products

--- server/controllers/productControllers.js
+++ server/controllers/productControllers.js
@@ -15,4 +15,30 @@ const createProduct = async (req, res) => {
   }
 };
 
-export { createProduct };
+const getProducts = async (req, res) => {
+  try {
+    const products = await pool.query("SELECT * FROM products");
+    res.json(products.rows);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
+
+const getProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await pool.query(
+      "SELECT * FROM products WHERE product_id = $1",
+      [id]
+    );
+    if (product.rows.length === 0) {
+      return res.json({ error: "Product does not exist" });
+    }
+
+    res.json(product.rows[0]);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
+
+export { createProduct, getProducts, getProduct };
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { createProduct } from "../controllers/productControllers.js";
+import {
+  createProduct,
+  getProduct,
+  getProducts,
+} from "../controllers/productControllers.js";
 
 const router = express.Router();
 
 router.route("/create").post(createProduct);
+router.route("/get").get(getProducts);
+router.route("/get/:id").get(getProduct);
 
 export default router;
