ec36682fdb55c937374a31cd3612c9d01abb9a25
albertying <albertlying@gmail.com>
2023-02-14 23:29:21 -0500

    Add versions to api routes

--- server/server.js
+++ server/server.js
@@ -13,8 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 //routes
-app.use("/auth", authenticationRoutes);
-app.use("/products", productRoutes);
+app.use("/api/v1/auth", authenticationRoutes);
+app.use("/api/v1/products", productRoutes);
 
 app.listen(5000, () => {
   console.log(`Server is running on port 5000.`);
