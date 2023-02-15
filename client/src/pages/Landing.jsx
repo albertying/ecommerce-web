483bd8516690e6f561f9927e7df10790daba8c94
albertying <albertlying@gmail.com>
2023-02-14 23:29:46 -0500

    Style product cards

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,15 +1,35 @@
 import "../css/landing.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Landing() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get(
+        "http://localhost:5000/api/v1/products/get"
+      );
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <div className="landing-container">
-      <div className="content-container">
-        <h1>Sell products easily</h1>
-        <p>
-          Lass tack jolly boat plunder boatswain rutters skysail Sea Legs keel
-          lateen sail. Spanker pinnace nipperkin parley red ensign cackle fruit
-          jolly boat lass yawl topsail.
-        </p>
+      <div className="product-container">
+        {products.map((product) => {
+          return (
+            <div className="product-card" key={product.product_id}>
+              <div className="product-text">
+                <h4>{product.product_name}</h4>
+                <p>{product.product_description}</p>
+                <p>${product.product_price}</p>
+              </div>
+              <div className="product-image"></div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
