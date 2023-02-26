dedefab3f0c8433ef2d9e6eafac506a53a1ac9f1
albertying <albertlying@gmail.com>
2023-02-26 00:39:51 -0500

    Add get route using getOrders controller

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createOrder } from "../controllers/orderControllers.js";
+import { createOrder, getOrders } from "../controllers/orderControllers.js";
 import verifyToken from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.route("/create").post(verifyToken, createOrder);
+router.route("/get").get(verifyToken, getOrders);
 
 export default router;
