bd83c5dd011f46c2d4bcfc9cb0c0bbcf7568d8df
albertying <albertlying@gmail.com>
2023-03-01 19:55:53 -0500

    Display cart items

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,8 +1,67 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { useUserContext } from "../context/UserContext";
+
+const Wrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CartWrapper = styled.div`
+  margin-top: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  height: 100%;
+  width: 50%;
+`;
+
+const CartCard = styled.div`
+  width: 40rem;
+  height: 10rem;
+  background-color: var(--light-gray);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
 function Cart() {
+  const [cart, setCart] = useState([]);
+
+  const { token } = useUserContext();
+
+  useEffect(() => {
+    const getCartCount = async () => {
+      const { data } = await axios.get(
+        "http://localhost:5000/api/v1/orders/get",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setCart(data);
+    };
+    getCartCount();
+  }, []);
+
   return (
-    <div>
-      <h1>Cart Page</h1>
-    </div>
+    <Wrapper>
+      <CartWrapper>
+        {cart.map((item) => {
+          return (
+            <CartCard key={item.order_id}>
+              <h1>{item.order_product_id}</h1>
+            </CartCard>
+          );
+        })}
+      </CartWrapper>
+    </Wrapper>
   );
 }
 export default Cart;
